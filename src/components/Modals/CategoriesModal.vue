7aefcbea3617b0934c3a0cfb4d83ba79d6dbff81
Ilya <napastyuk@gmail.com>
2026-05-24 01:21:50 +0300

    adding category modal

---
+++ src/components/Modals/CategoriesModal.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { ref } from 'vue'
+import Dialog from 'primevue/dialog'
+import InputText from 'primevue/inputtext'
+import Button from 'primevue/button'
+import { Form } from '@primevue/forms';
+
+const modelValue = defineModel()
+
+const categoryName = ref('');
+const listCategory = ref([
+  {
+    id: 1,
+    name: 'Тестовая категория'
+  },
+  {
+    id: 2,
+    name: 'Тестовая категория 2'
+  }
+])
+
+const saveCategory = () => {
+  console.log('saveCategory')
+}
+
+const deleteCategory = (id) => {
+  console.log('deleteCategory ', id)
+}
+</script>
+
+<template>
+  <Dialog header="Категория" v-model:visible="modelValue" :style="{ width: '25rem' }">
+    <Form @submit="saveCategory">
+      <div class="gap-2 mb-2">
+        <InputText v-model="categoryName" class="flex-auto w-full" autocomplete="off"
+          placeholder="Название новой категории" />
+      </div>
+      <div class="flex justify-end gap-2 mt-4">
+        <Button type="button" label="Добавить" @click="saveCategory" />
+      </div>
+      <div class="flex justify-between items-center mt-3 mb-1 gap-5"
+        v-for="category in listCategory"
+        :key="category.id">
+        {{ category.name }}
+        <Button type="button" rounded size="small" variant="text" icon="pi pi-times"
+          @click="deleteCategory(category.id)" />
+      </div>
+    </Form>
+  </Dialog>
+</template>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import { useRouter } from 'vue-router'
 import Button from 'primevue/button'
 import Avatar from 'primevue/avatar'
@@ -7,11 +7,14 @@ import Menubar from 'primevue/menubar'
 import { useUserStore } from '@/stores/userStore'
 import { useAuth } from '@/composables/useAuth'
 import { useToastNotification } from '@/composables/useToastNotifications'
+import CategoriesModal from './Modals/CategoriesModal.vue'
 
 const authStore = useUserStore()
 const router = useRouter()
 const { showToast } = useToastNotification()
 const { signOut, errorMessage } = useAuth()
+
+const CategoriesDialogVisible = ref(false)
 
 const emailFirstLetter = computed(() => {
   return authStore.user?.email ? authStore.user?.email[0].toUpperCase() : ''
@@ -30,6 +33,7 @@ const signOutUser = async () => {
 </script>
 
 <template>
+  <CategoriesModal v-model="CategoriesDialogVisible"/>
   <div class="mb-5">
     <Menubar>
       <template #start>
@@ -37,7 +41,7 @@ const signOutUser = async () => {
           <span class="font-bold">Link Manager</span>
           <div class="flex item-center gap-2">
             <Button icon="pi pi-link" rounded />
-            <Button icon="pi pi-folder" rounded />
+            <Button icon="pi pi-folder" rounded @click="CategoriesDialogVisible=true" />
           </div>
         </div>
       </template>
